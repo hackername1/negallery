533a810478fd288e997966201b0c0f0ed6504a1a
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-05-04 13:54:14 +0300

    Add constants.

--- backend/server/globals.go
+++ backend/server/globals.go
@@ -8,5 +8,21 @@ var myEnvironment map[string]string
 
 // Constants
 const envPath = ".env"
+const tempPath = "/temp"
 const serverPort = ":8443"
 const apiUrl = "/api/v1"
+const serverErrorString = "Error on the server"
+
+const (
+	widthOrigin = 0
+	width1920   = 1920
+	width1280   = 1280
+	width640    = 640
+)
+
+var widths = []int{
+	widthOrigin,
+	width1920,
+	width1280,
+	width640,
+}
